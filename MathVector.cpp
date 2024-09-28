c37dff6a58ea200e8a19f28edb83a64a47553dcf
Mingze Lee <lmz24@mails.tsinghua.edu.cn>
2024-09-28 16:01:11 +0800

    Update MathVector.cpp

--- MathVector.cpp
+++ MathVector.cpp
@@ -9,50 +9,52 @@
 #include "MathVector.h"
 #include <cmath>
 
-
-MVector::MVector(double x, double y)
+namespace MVECTOR
 {
-    x_ = x;
-    y_ = y;
-}
+    MVector::MVector(double x, double y)
+    {
+        x_ = x;
+        y_ = y;
+    }
 
-MVector::~MVector() { };
+    MVector::~MVector() { };
+    
+    void MVector::Reset(const double a,const double b)
+    {
+        x_ = a;
+        y_ = b;
+    }
 
-void MVector::Reset(const double a,const double b)
-{
-    x_ = a;
-    y_ = b;
-}
+    MVector MVector::operator+(const MVector & v) const
+    {
+        double x_co, y_co;
+        x_co = x_ + v.x_;
+        y_co = y_ + v.y_;
+        return MVector(x_co, y_co);
+    }
 
-MVector MVector::operator+(const MVector & v) const
-{
-    double x_co, y_co;
-    x_co = x_ + v.x_;
-    y_co = y_ + v.y_;
-    return MVector(x_co, y_co);
-}
+    
+    MVector MVector::operator*(const double a) const
+    {
+        double x_co, y_co;
+        x_co = x_ * a;
+        y_co = y_ * a;
+        return MVector(x_co, y_co);
+    }
 
-
-MVector MVector::operator*(const double a) const
-{
-    double x_co, y_co;
-    x_co = x_ * a;
-    y_co = y_ * a;
-    return MVector(x_co, y_co);
-}
-
-double MVector::operator*(const MVector & v) const
-{
-    return x_ * v.x_ + y_ * v.y_;
-}
-
-double MVector::norm() const
-{
-    return sqrt(*this * *this);
-}
-
-std::ostream & operator<<(std::ostream & os, const MVector & v)
-{
-    os << "(" << v.x_ << ", " << v.y_ << ")";
-    return os;
+    double MVector::operator*(const MVector & v) const
+    {
+        return x_ * v.x_ + y_ * v.y_;
+    }
+    
+    double MVector::norm() const
+    {
+        return sqrt(*this * *this);
+    }
+    
+    std::ostream & operator<<(std::ostream & os, const MVector & v)
+    {
+        os << "(" << v.x_ << ", " << v.y_ << ")";
+        return os;
+    }
 }
